19a6a34877abfb3b3752d47f5cba065af6b8a1ef
congenial-course-2234 <jakelozano94@gmail.com>
2020-05-13 20:36:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,18 +10,14 @@ def using_pop(array)
   array.pop
 end
 
-def pop_with_args(array, arg)
-  arg.times do
-  array.pop
-end
+def pop_with_args(array)
+  array.pop(2)
 end
 
 def using_shift(array)
   array.shift
 end
 
-def shift_with_args(array, arg)
-  arg.times do
-  array.shift
-end
+def shift_with_args(array)
+  array.shift(2)
 end
